f11c08677680e2c35b5f0162ba7bb58a183854d2
Illumias <oliver.varnild@live.dk>
2018-11-27 13:09:19 +0100

    Update Meeting.java

--- Meeting.java
+++ Meeting.java
@@ -1,1 +1,21 @@
+public class Meeting extends Event {
 
+    @Override
+    public void read() {
+
+    }
+    @Override
+    public void exportData() {
+        System.out.println(this + " was exported as a " + getClass());
+    }
+    @Override
+    public void importData() {
+
+    }
+
+    public Meeting() {
+        Filehandling.autoAddExportable(this);
+    }
+
+
+}

aaebfc4de51fc79cd6ad791dec4ceb5b5c7673b6
Illumias <oliver.varnild@live.dk>
2018-11-27 13:09:39 +0100

    Update Transport.java

--- Transport.java
+++ Transport.java
@@ -1,1 +1,20 @@
+public class Transport extends Event {
 
+    @Override
+    public void read() {
+
+    }
+    @Override
+    public void exportData() {
+        System.out.println(this + " was exported a " + getClass());
+    }
+    @Override
+    public void importData() {
+
+    }
+
+    public Transport() {
+        Filehandling.autoAddExportable(this);
+    }
+
+}

70d628de28162c8fb6acb9f7ce3aa2854647bc3a
Illumias <oliver.varnild@live.dk>
2018-11-27 13:08:41 +0100

    Update Facilitator.java

--- Facilitator.java
+++ Facilitator.java
@@ -1,1 +1,20 @@
+public class Facilitator implements Readable, Exportable {
 
+    @Override
+    public void read() {
+
+    }
+    @Override
+    public void exportData() {
+        System.out.println(this + " was exported as a " + getClass());
+    }
+    @Override
+    public void importData() {
+
+    }
+
+    public Facilitator() {
+        Filehandling.autoAddExportable(this);
+    }
+
+}

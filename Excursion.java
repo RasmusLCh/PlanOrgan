14e9b0b8e3b6e6d138637c5e7b42e91c27322d49
Illumias <oliver.varnild@live.dk>
2018-11-27 13:08:13 +0100

    Update Excursion.java

--- Excursion.java
+++ Excursion.java
@@ -1,1 +1,20 @@
+public class Excursion extends Event {
 
+    @Override
+    public void read() {
+
+    }
+    @Override
+    public void exportData() {
+        System.out.println(this + " was exported as a " + getClass());
+    }
+    @Override
+    public void importData() {
+
+    }
+
+    public Excursion() {
+        Filehandling.autoAddExportable(this);
+    }
+
+}

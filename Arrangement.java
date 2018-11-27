1487fe68adf974b6ce17bb47165cecf3c6863c1e
Illumias <oliver.varnild@live.dk>
2018-11-27 13:07:30 +0100

    Update Arrangement.java

--- Arrangement.java
+++ Arrangement.java
@@ -1,1 +1,20 @@
+public class Arrangement implements Readable, Exportable {
 
+    @Override
+    public void read() {
+
+    }
+    @Override
+    public void exportData() {
+        System.out.println(this + " was exported as a " + getClass());
+    }
+    @Override
+    public void importData() {
+
+    }
+
+    public Arrangement() {
+        Filehandling.autoAddExportable(this);
+    }
+
+}
